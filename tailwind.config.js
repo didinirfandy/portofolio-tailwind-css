03597d366f710abd5e52937471bd385c0662cc6e
didinirfandy <didinirfnady16@gmail.com>
2022-04-16 05:11:50 +0700

    update dark mode dan perbaikan fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-  content: ['index.html'],
-  theme: {
-    container: {
-      center: true,
-      padding: '16px',
-
+    content: ["index.html"],
+    darkMode: "class",
+    theme: {
+        container: {
+            center: true,
+            padding: "16px",
+        },
+        extend: {
+            colors: {
+                primary: "#00bcd4",
+                secondary: "#64748b",
+                secondarybg: "#a7b5c9",
+                dark: "#0f172a",
+            },
+            screens: {
+                "2cl": "1320px",
+            },
+        },
     },
-    extend: {
-      colors: {
-        'primary': '#00bcd4',
-        'secondary': '#64748b',
-        'dark': '#0f172a',
-      },
-      screens: {
-        '2cl': '1320px',
-      }
-    },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
