520337b90a938bdc89c39fc8f72b35994e2d9d99
didinirfandy <didinirfnady16@gmail.com>
2022-04-16 07:18:26 +0700

    update screens yg salah

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    content: ["index.html"],
-    darkMode: "class",
+    content: ['index.html'],
+    darkMode: 'class',
     theme: {
         container: {
             center: true,
-            padding: "16px",
+            padding: '16px',
         },
         extend: {
             colors: {
-                primary: "#00bcd4",
-                secondary: "#64748b",
-                secondarybg: "#a7b5c9",
-                dark: "#0f172a",
+                primary: '#00bcd4',
+                secondary: '#64748b',
+                secondarybg: '#a7b5c9',
+                dark: '#0f172a',
             },
             screens: {
-                "2cl": "1320px",
+                '2xl': '1320px',
             },
         },
     },
